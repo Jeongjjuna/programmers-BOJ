8c57b169e54c0b327561da1c53acf3952254c940
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2024-02-14 09:34:11 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+
+
+def find_fail_rate(i, count, prefix_sum):
+    if prefix_sum[i] == 0:
+        return 0
+    return count[i] / prefix_sum[i]
+    
+
+def solution(N, stages):
+    
+    # 각 스테이지에 멈춰있는 사람의 수를 카운트 한다.
+    # count = [1, 3, 2, 1, 0 ,1]
+    count = [0] * (N + 2)
+    for s in stages:
+        count[s] += 1
+    
+    # 뒤에서 부터 누적합을 구한다.
+    # prefix_sum = [8, 7, 4, 2, 1, 1]
+    prefix_sum = [0] * (N + 2)
+    prefix_sum[N + 1] = count[N + 1]
+    for i in range(N, 0, -1):
+        prefix_sum[i] = prefix_sum[i + 1] + count[i]
+    
+    # i번 스테이지와, i번 스테이지의 실패율을 쌍으로 구한다.
+    fail_rates = []
+    for i in range(1, N + 1):
+        # i번 스테이지의 실패율을 구한다.
+        fail_rate = find_fail_rate(i, count, prefix_sum)
+        fail_rates.append((fail_rate, i))
+    
+    
+    # 정렬
+    # 1. 실패율 내림차순
+    # 2. 같으면 스테이지 작은 순으로 정렬
+    fail_rates.sort(key = lambda x : (-x[0], x))
+
+    
+    return [elem[1] for elem in fail_rates]
+
+
+
+
+
