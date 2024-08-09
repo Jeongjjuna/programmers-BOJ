ce089f2e9c88f324f4528270e5366aeaf17e0891
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2024-08-09 17:44:39 +0900

    [level 2] Title: 타겟 넘버, Time: 15.52 ms, Memory: 59.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=kotlin) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=kotlin#) 
 
 ### 성능 요약
 
-메모리: 59.6 MB, 시간: 8.64 ms
+메모리: 59.2 MB, 시간: 15.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 09일 17:29:05
+2024년 08월 09일 17:44:33
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.kt
@@ -1,4 +1,4 @@
-// 방법1. 완전탐색
+// 방법1. 완전탐색 + dfs
 // 2**20 = 1048576
 
 class Solution {
@@ -6,18 +6,20 @@ class Solution {
     private var answer = 0
     
     fun solution(numbers: IntArray, target: Int): Int {        
-        dfs(numbers, target, 0, 0)
-        return this.answer
+        dfs(numbers = numbers, target = target)
+        return answer
     }
     
-    fun dfs(numbers: IntArray, target: Int, sum: Int, depth: Int) {
+    fun dfs(numbers: IntArray, target: Int, sum: Int = 0, depth: Int = 0) {
+        // 종료조건
         if (depth == numbers.size) {
             if (sum == target) {
-                this.answer += 1
+                answer ++
             }
             return
         }
         
+        // 재귀 탐색
         dfs(numbers, target, sum + numbers[depth], depth + 1)
         dfs(numbers, target, sum - numbers[depth], depth + 1)
     }
