c6b0c2b954be7939f7f09f4e044c02758f804e73
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2024-10-13 07:31:20 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1080 ms, Memory: 249160 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.kt
@@ -0,0 +1,23 @@
+fun main() = with(System.`in`.bufferedReader()) {
+
+    // 변수 입력 및 선언
+    val n = readLine().toInt()
+    val cards = readLine().split(" ").map { it.toInt() }
+    val m = readLine().toInt()
+    val targets = readLine().split(" ").map { it.toInt() }
+    val count = mutableMapOf<Int, Int>()
+
+    // 카운팅
+    for (card in cards) {
+        count[card] = count.getOrDefault(card, 0) + 1
+    }
+
+    // 개수 출력
+    val answer = StringBuilder()
+    for (target in targets) {
+        val cnt = count.getOrDefault(target, 0)
+        answer.append(cnt).append(" ")
+    }
+
+    println(answer)
+}
