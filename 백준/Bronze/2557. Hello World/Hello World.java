db8974673d7dce7faf68006ada5ef950a89e7342
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2025-01-23 22:05:54 +0900

    [Bronze V] Title: Hello World, Time: 100 ms, Memory: 13968 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder sb = new StringBuilder();
+
+    static void input() throws IOException {
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
