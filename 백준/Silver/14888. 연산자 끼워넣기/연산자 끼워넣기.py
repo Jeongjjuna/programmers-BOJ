afb1bf78d67c69cf1486200451b9b7ddc2465cea
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2025-12-28 17:30:31 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 7180 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,58 +1,67 @@
-import sys; input = sys.stdin.readline
-from itertools import permutations
+import sys
+input = sys.stdin.readline
 
-# 입력
 n = int(input())
-arr = list(map(int, input().split()))
-num_four = list(map(int, input().split())) # +, -, x, % 의 개수
+nums = list(map(int, input().split()))
+operator = list(map(int, input().split()))
+min_result = sys.maxsize
+max_result = -sys.maxsize
+
+# 0 덧셈, 1 뺼샘, 2 곱셈, 3 나눗셈
+arr = []
+for i in range(4):
+    count = operator[i]
+    for _ in range(count):
+        arr.append(i)
 
 '''
-수열의 순서를 바꾸면 안됨!!
-연산자 우선순위를 무시하고 앞에서부터 계산한다!!
-    나눗셈 할 때 몫(정수)만 취한다.
-    음수를 양수로 나눌 때는 양수로 바꾸고 몫을 구하고, 그 몫을 음수로 바꾼다.
-
-계산 결과의 최대와 최소를 구하라.
-
-최대 O(10!) * O(n)
-
+중복이 혀용 배열의 순열 구하기
+ex) 0 0 1 1 2 3 3
 '''
 
-# per_list = 연산자 순서경우의 수 구하기
-com = []
-for i, x in enumerate(num_four):
-    for j in range(x):
-        com.append(i)
+def calculate(operators):
+    # nums      [1, 2, 3, 4, 5, 6]
+    # operators [3, 2, 0, 1, 0]
+    num1 = nums[0]
+    for i in range(0, len(nums) - 1):
+        num2 = nums[i + 1]
+        operator = operators[i]
 
-per_list = []
-for p in permutations(com, len(com)):
-    per_list.append(p)
-
-# per_list를 순회하며 각 연산의 최대, 최소값을 갱신해주기
-ans_max, ans_min = -sys.maxsize, sys.maxsize
-for x in per_list: #O(300만)
-    # x = (0, 0, 1, 2, 3)
-    # arr = 1 2 3 4 5 6
-    ans = arr[0]
-    for i in range(len(arr)-1):
-        if x[i] == 0:
-            ans = ans + arr[i+1]
-            
-        elif x[i] == 1:
-            ans = ans - arr[i+1]
-
-        elif x[i] == 2:
-            ans = ans * arr[i+1]
-
-        else:
-            if ans < 0:
-                ans = (-ans) // arr[i+1]
-                ans = -(ans)
+        if (operator == 0):
+            num1 = num1 + num2
+        elif (operator == 1):
+            num1 = num1 - num2
+        elif (operator == 2):
+            num1 = num1 * num2
+        elif (operator == 3):
+            if num1 < 0:
+                num1 = -num1
+                num1 = num1 //num2
+                num1 = -num1
             else:
-                ans = ans // arr[i+1]
+                num1 = num1 // num2
+    return num1
 
-    ans_max = max(ans_max, ans)
-    ans_min = min(ans_min, ans)
+def dfs(visited, curr):
+    global min_result, max_result
 
-print(ans_max)
-print(ans_min)
+    if len(curr) == n - 1:
+        result = calculate(curr)
+        min_result, max_result = min(min_result, result), max(max_result, result)
+        return
+
+    for i in range(len(arr)):
+        if not visited[i]:
+            visited[i] = True
+            curr.append(arr[i])
+            dfs(visited, curr)
+            visited[i] = False
+            curr.pop()
+
+def main():
+    dfs([False] * (n - 1), [])
+    print(max_result)
+    print(min_result)
+
+if __name__ == "__main__":
+    main()
