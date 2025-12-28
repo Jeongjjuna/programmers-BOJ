f8fe7b1fdc637f345a93cdff8530e47d2cce138c
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2025-12-28 18:43:40 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,67 +1,58 @@
 import sys
+
+sys.setrecursionlimit(10 ** 7)
 input = sys.stdin.readline
 
 n = int(input())
 nums = list(map(int, input().split()))
 operator = list(map(int, input().split()))
-min_result = sys.maxsize
-max_result = -sys.maxsize
-
-# 0 덧셈, 1 뺼샘, 2 곱셈, 3 나눗셈
-arr = []
-for i in range(4):
-    count = operator[i]
-    for _ in range(count):
-        arr.append(i)
+min_val = sys.maxsize
+max_val = -sys.maxsize
 
 '''
 중복이 혀용 배열의 순열 구하기
 ex) 0 0 1 1 2 3 3
 '''
 
-def calculate(operators):
-    # nums      [1, 2, 3, 4, 5, 6]
-    # operators [3, 2, 0, 1, 0]
-    num1 = nums[0]
-    for i in range(0, len(nums) - 1):
-        num2 = nums[i + 1]
-        operator = operators[i]
 
-        if (operator == 0):
-            num1 = num1 + num2
-        elif (operator == 1):
-            num1 = num1 - num2
-        elif (operator == 2):
-            num1 = num1 * num2
-        elif (operator == 3):
-            if num1 < 0:
-                num1 = -num1
-                num1 = num1 //num2
-                num1 = -num1
-            else:
-                num1 = num1 // num2
-    return num1
+def calculate(operand1: int, operand2: int, operator: int) -> int:
+    if (operator == 0):
+        return operand1 + operand2
+    elif (operator == 1):
+        return operand1 - operand2
+    elif (operator == 2):
+        return operand1 * operand2
+    else:
+        if operand1 < 0:
+            return -((-operand1) // operand2)
+        else:
+            return operand1 // operand2
 
-def dfs(visited, curr):
-    global min_result, max_result
 
-    if len(curr) == n - 1:
-        result = calculate(curr)
-        min_result, max_result = min(min_result, result), max(max_result, result)
+def dfs(dep: int, cal_result: int, visited: list):
+    # 연산자를 전부 사용했다면
+    if dep == n:
+        global max_val, min_val
+        max_val = max(max_val, cal_result)
+        min_val = min(min_val, cal_result)
         return
 
-    for i in range(len(arr)):
-        if not visited[i]:
-            visited[i] = True
-            curr.append(arr[i])
-            dfs(visited, curr)
-            visited[i] = False
-            curr.pop()
+    # 0 덧셈, 1 뺄셈, 2 곱셈, 3 나눗셈
+    for i in range(4):
+        operator_cnt = operator[i]
+        if operator_cnt != 0:
+            operator[i] -= 1
+            visited.append(i)
+            dfs(dep + 1, calculate(cal_result, nums[dep], i), visited)
+            operator[i] += 1
+            visited.pop()
+
 
 def main():
-    dfs([False] * (n - 1), [])
-    print(max_result)
-    print(min_result)
+    dfs(1, nums[0], [])
+    print(max_val)
+    print(min_val)
+
 
 if __name__ == "__main__":
     main()
