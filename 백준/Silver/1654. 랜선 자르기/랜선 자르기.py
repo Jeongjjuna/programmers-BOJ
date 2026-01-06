65ea81a3886795fe7896baac00c6cb25af06e995
정지훈 <87998104+Jeongjjuna@users.noreply.github.com>
2026-01-06 22:09:42 +0900

    [Silver II] Title: 랜선 자르기, Time: 56 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,25 +1,40 @@
-import sys; input = sys.stdin.readline
+import sys
+
+input = sys.stdin.readline
+
+# 변수 및 입력 선언
+k, n = map(int, input().split())
+arr = []
+for _ in range(k):
+    arr.append(int(input()))
+
+
+def main():
+    # 오름차순
+    arr.sort()
+
+    # 이진탐색으로 자를 수 있는 최대 길이를 구하자. 최대 길이 -> 최대로 많은 개수
+    answer = 0
+    start, end = 0, 2 ** 31 - 1
+    while start <= end:
+
+        mid = (start + end) // 2
+
+        # mid 길이로 전부 나눴을 때 가능한지?
+        cnt = 0
+        for elem in arr:
+            cnt += (elem // mid)
+
+        if n <= cnt:
+            # 가능
+            start = mid + 1
+            answer = max(answer, mid)
+        else:
+            # 불가능
+            end = mid - 1
+
+    print(answer)
 
 
 if __name__ == "__main__":
-    k, n = map(int, input().split())
-    arr = [int(input()) for _ in range(k)]
-
-    # 이진탐색을 진행합니다.
-    left, right = 1, max(arr)
-    ans = 0
-    while left <= right:
-        mid = (left + right) // 2
-        
-        # mid로 잘랐을 때 나오는 개수
-        cnt = 0
-        for elem in arr:
-            cnt += elem // mid
-
-        if cnt >= n:
-            ans = max(ans, mid)
-            left = mid + 1  
-        else:
-            right = mid - 1
-
-    print(ans)
+    main()
